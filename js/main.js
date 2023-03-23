9e515783d7e67fb2e10061668d0ea1e471b1d79f
Arseniy Graur <me@argraur.dev>
2023-03-23 14:41:45 +0300

    task4

--- js/main.js
+++ js/main.js
@@ -5,3 +5,15 @@ const randomIntInRange = (left, right) => {
 }
 
 const checkStrLength = (str, maxLength) => str.length <= maxLength
+
+const createObject = (i, desc) => ({
+    id: i,
+    url: `photos/${i}.jpg`,
+    description: desc,
+    likes: randomIntInRange(15, 200),
+    comments: randomIntInRange(0, 200)
+})
+
+let objArray = []
+for (let i = 0; i < 25; i++)
+    objArray.push(createObject(i, `Object №${i}`))
