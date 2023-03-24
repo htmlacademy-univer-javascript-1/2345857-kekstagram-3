a9d67c82935c98b085d7035227fac2ef2bd34e58
Arseniy Graur <me@argraur.dev>
2023-03-24 21:51:45 +0300

    module7-task1

--- js/data.js
+++ js/data.js
@@ -2,7 +2,7 @@ import { createObject } from './utils.js';
 
 const generateObjects = () => {
   const objArray = [];
-  for (let i = 0; i < 25; i++) {
+  for (let i = 1; i <= 25; i++) {
     objArray.push(createObject(i, `Object №${i}`));
   }
   return objArray;
